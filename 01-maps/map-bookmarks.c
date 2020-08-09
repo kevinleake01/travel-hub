87109686d971e24cd26aea521c64b4a7d7de2dac
Kevin Leake <kevinleake01@gmail.com>
2020-08-09 21:56:39 +0000

    Add files via upload

---
+++ 01-maps/map-bookmarks.c
@@ -0,0 +1,77 @@
+/*
+####################################
+#
+# --- MAP-BOOKMARKS.C ---
+#
+# This is a tool to quickly set your co-ordinates
+# using decimal Lat-Long Degrees.
+# 
+# Example Usage:
+# 
+# gcc mapbookmarks.c; ./a.out "51.5122,-0.0904" > map-bookmarks-london.html
+# gcc mapbookmarks.c; ./a.out "48.8563,2.3485" > map-bookmarks-paris.html
+# gcc mapbookmarks.c; ./a.out "40.7161,-73.9912" > map-bookmarks-newyork.html
+#
+####################################
+*/
+
+#include <stdio.h>
+
+/* --- Function Prototypes go here --- */
+int main(int argc, char *argv[]);
+
+/* --- Global variables go here --- */
+
+int main(int argc, char *argv[])
+{
+  printf("<!DOCTYPE html>\n");
+  printf("<html lang=\"en\">\n");
+  printf("<head>\n");
+  printf("<meta charset=\"utf-8\" >\n");
+  printf("<title>Map Bookmarks</title>\n");
+  printf("<style>\n");
+  printf("body {\n");
+  printf("  background-color: #000000;\n");
+  printf("  color: #FFFFFF;\n");
+  printf("}\n");
+  printf("h1 {\n");
+  printf("  background-color: #585858;\n");
+  printf("  color: #FFFFFF;\n");
+  printf("}\n");
+  printf("h2 {\n");
+  printf("  background-color: #585858;\n");
+  printf("  color: #FFFFFF;\n");
+  printf("}\n");
+  printf("a:link {\n");
+  printf("  color: #FF0000;\n");
+  printf("}\n");
+  printf("a:visited {\n");
+  printf("  color: #00FF00;\n");
+  printf("}\n");
+  printf("</style>\n");
+  printf("</head>\n");
+  printf("<body>\n");
+  printf("<h1>Map Bookmarks</h1>\n");
+  printf("<h2>For location (Lat-Long): %s</h2>\n", argv[1]);
+  printf("<p>\n");
+  printf("<a href=\"https://www.3geonames.org/%s\">3Geonames</a><br>\n", argv[1]);
+  printf("<a href=\"https://api.3geonames.org/%s\">3Geonames API</a><br>\n", argv[1]);
+  printf("<a href=\"https://geocode.xyz/%s\">Geocode.xyz</a><br>\n", argv[1]);
+  printf("<a href=\"https://geocode.xyz/%s?geoit=xml\">Geocode.xyz XML</a><br>\n", argv[1]);
+  printf("<a href=\"http://geohash.org?q=%s&format=url&redirect=0\">Geohash: URL</a><br>\n", argv[1]);
+  printf("<a href=\"http://geohash.org?q=%s&format=osm\">Geohash: OpenStreetmap</a><br>\n", argv[1]);
+  printf("<a href=\"http://geohash.org?q=%s&format=gmaps\">Geohash: Google Maps</a><br>\n", argv[1]);
+  printf("<a href=\"https://api.mapcode.com/mapcode/codes/%s\">Mapcode REST-API</a><br>\n", argv[1]);
+  printf("<a href=\"https://plus.codes/api?address=%s\">Plus Codes API</a><br>\n", argv[1]);
+  printf("<a href=\"https://www.geoplaner.com/?z=12;p=%s\">Geoplaner</a><br>\n", argv[1]);
+  printf("<a href=\"https://wego.here.com/?x=ep&map=%s,10,normal\">Here We Go</a><br>\n", argv[1]);
+  printf("<a href=\"https://mappingsupport.com/p2/gissurfer.php?center=%s&zoom=10&basemap=ESRI_Topographic\">Mappingsupport: ESRI Topographic</a><br>\n", argv[1]);
+  printf("<a href=\"https://mappingsupport.com/p2/gissurfer.php?center=%s&zoom=10&basemap=ESRI_Streets\">Mappingsupport: ESRI Streets</a><br>\n", argv[1]);
+  printf("<a href=\"https://mappingsupport.com/p2/gissurfer.php?center=%s&zoom=10&basemap=ESRI_Aerial\">Mappingsupport: ESRI Aerial</a><br>\n", argv[1]);
+  printf("<a href=\"https://mapper.acme.com/?ll=%s&z=15&t=M&marker0=%s\">ACME Mapper</a><br>\n", argv[1], argv[1]);
+  printf("<a href=\"https://www.mapquest.com/latlng/%s?zoom=15&maptype=map\">Mapquest</a><br>\n", argv[1]);
+  printf("</p>\n");
+  printf("</body>\n");
+  printf("</html>\n");
+}
+
