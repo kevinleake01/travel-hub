61a54d6aae83fae1d300d12b677def3fc5ecc155
Kevin Leake <kevinleake01@gmail.com>
2020-08-10 17:35:26 +0000

    Update map-bookmarks.c

--- 01-maps/map-bookmarks.c
+++ 01-maps/map-bookmarks.c
@@ -8,9 +8,9 @@
 # 
 # Example Usage:
 # 
-# gcc mapbookmarks.c; ./a.out "51.5122,-0.0904" > map-bookmarks-london.html
-# gcc mapbookmarks.c; ./a.out "48.8563,2.3485" > map-bookmarks-paris.html
-# gcc mapbookmarks.c; ./a.out "40.7161,-73.9912" > map-bookmarks-newyork.html
+# gcc map-bookmarks.c; ./a.out "51.5122,-0.0904" > map-bookmarks-london.html
+# gcc map-bookmarks.c; ./a.out "48.8563,2.3485" > map-bookmarks-paris.html
+# gcc map-bookmarks.c; ./a.out "40.7161,-73.9912" > map-bookmarks-newyork.html
 #
 ####################################
 */
@@ -63,13 +63,18 @@ int main(int argc, char *argv[])
   printf("<a href=\"http://geohash.org?q=%s&format=gmaps\">Geohash: Google Maps</a><br>\n", argv[1]);
   printf("<a href=\"https://api.mapcode.com/mapcode/codes/%s\">Mapcode REST-API</a><br>\n", argv[1]);
   printf("<a href=\"https://plus.codes/api?address=%s\">Plus Codes API</a><br>\n", argv[1]);
-  printf("<a href=\"https://www.geoplaner.com/?z=12;p=%s\">Geoplaner</a><br>\n", argv[1]);
+  printf("<a href=\"https://www.geoplaner.com/?z=10;p=%s;;\">Geoplaner</a><br>\n", argv[1]);
   printf("<a href=\"https://wego.here.com/?x=ep&map=%s,10,normal\">Here We Go</a><br>\n", argv[1]);
   printf("<a href=\"https://mappingsupport.com/p2/gissurfer.php?center=%s&zoom=10&basemap=ESRI_Topographic\">Mappingsupport: ESRI Topographic</a><br>\n", argv[1]);
   printf("<a href=\"https://mappingsupport.com/p2/gissurfer.php?center=%s&zoom=10&basemap=ESRI_Streets\">Mappingsupport: ESRI Streets</a><br>\n", argv[1]);
   printf("<a href=\"https://mappingsupport.com/p2/gissurfer.php?center=%s&zoom=10&basemap=ESRI_Aerial\">Mappingsupport: ESRI Aerial</a><br>\n", argv[1]);
   printf("<a href=\"https://mapper.acme.com/?ll=%s&z=15&t=M&marker0=%s\">ACME Mapper</a><br>\n", argv[1], argv[1]);
   printf("<a href=\"https://www.mapquest.com/latlng/%s?zoom=15&maptype=map\">Mapquest</a><br>\n", argv[1]);
+  printf("<a href=\"http://boulter.com/gps/?c=%s\">Boulter Geocaching</a><br>\n", argv[1]);
+  printf("<a href=\"https://www.gpsvisualizer.com/draw/?bg=OPENSTREETMAP&z=10&c=%s\">GPS Visualizer: Openstreetmap</a><br>\n", argv[1]);
+  printf("<a href=\"https://www.gpsvisualizer.com/draw/?bg=OPENTOPOMAP&z=10&c=%s\">GPS Visualizer: Opentopomap</a><br>\n", argv[1]);
+  printf("<a href=\"https://www.gpsvisualizer.com/draw/?bg=KOMOOT_OSM&z=10&c=%s\">GPS Visualizer: OSM Komoot</a><br>\n", argv[1]);
+  printf("<a href=\"https://www.gpsvisualizer.com/draw/?bg=ARCGIS_HYBRID&z=10&c=%s\">GPS Visualizer: ArcGIS Hybrid</a><br>\n", argv[1]);
   printf("</p>\n");
   printf("</body>\n");
   printf("</html>\n");
