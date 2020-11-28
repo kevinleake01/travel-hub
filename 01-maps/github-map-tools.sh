f115fab7008ce14f23d2bc67890102434a6b3154
Kevin Leake <kevinleake01@gmail.com>
2020-11-28 17:53:12 +0000

    Create github-map-tools.sh

---
+++ 01-maps/github-map-tools.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Geohash
+git clone git://github.com/vinsci/geohash.git 0001-geohash
+
+# 3geonames and geocode.xyz
+git clone git://github.com/eruci/geocode.git 0002-geocode
+
+# Mapcode
+git clone git://github.com/mapcode-foundation/mapcode-cpp.git 0003-mapcode-cpp
+git clone git://github.com/mapcode-foundation/mapcode-rest-service.git 0003-mapcode-rest-service
+
+# Open Location Codes / Plus Codes
+git clone git://github.com/google/open-location-code.git 0004-pluscode
+
+# MGRS
+git clone git://github.com/sotex/geographiclib.git 0005-mgrs-geographiclib
+git clone git://github.com/hobu/mgrs.git 0005-mgrs-hobu
+
+# QGIS
+git clone git://github.com/NationalSecurityAgency/qgis-latlontools-plugin.git 0006-qgis-latlongtools
+
