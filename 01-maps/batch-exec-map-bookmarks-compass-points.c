60e9160c3c8e9305e45436a4ecb649397ebfcebc
Kevin Leake <kevinleake01@gmail.com>
2021-12-12 11:12:52 +0000

    Add files via upload

---
+++ 01-maps/batch-exec-map-bookmarks-compass-points.c
@@ -0,0 +1,27 @@
+/*
+####################################
+#
+# --- BATCH-EXEC-MAP-BOOKMARKS-COMPASS-POINTS.C ---
+#
+# To run this program:
+#
+# gcc batch-exec-map-bookmarks-compass-points.c; ./a.out
+#
+####################################
+*/
+
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+  system("gcc map-bookmarks.c -o a.out-1\n");
+  system("mkdir map-bookmarks-compass-points\n");
+  system("./a.out-1 0.0,0.0 > map-bookmarks-compass-points/000.html\n");
+  system("./a.out-1 89.999999,0.0 > map-bookmarks-compass-points/north.html\n");
+  system("./a.out-1 -89.999999,0.0 > map-bookmarks-compass-points/south.html\n");
+  system("./a.out-1 0.0,90.0 > map-bookmarks-compass-points/east.html\n");
+  system("./a.out-1 0.0,-90.0 > map-bookmarks-compass-points/west.html\n");
+  system("./a.out-1 0.0,-179.999999 > map-bookmarks-compass-points/180.html\n");
+  system("rm a.out-1\n");
+}
+
---
+++ 01-maps/batch-exec-map-bookmarks-users-20211217.c
@@ -0,0 +1,58 @@
+/*
+####################################
+#
+# --- BATCH-EXEC-MAP-BOOKMARKS-USERS-20211217.C ---
+#
+# To run this program:
+#
+# gcc batch-exec-map-bookmarks-users-20211217.c; ./a.out
+#
+####################################
+*/
+
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+  system("gcc map-bookmarks.c -o a.out-1\n");
+  system("mkdir map-bookmarks-users-20211217\n");
+  system("./a.out-1 62.14832,65.43485 > map-bookmarks-users-20211217/20210118-2152-oasis-plaza.html\n");
+  system("./a.out-1 65.53717,72.53168 > map-bookmarks-users-20211217/20210118-2154-life-nadym.html\n");
+  system("./a.out-1 65.85556,74.49014 > map-bookmarks-users-20211217/20210118-2157-cafe-viva.html\n");
+  system("./a.out-1 66.11597,76.68394 > map-bookmarks-users-20211217/20210118-2208-cafe-maria-mia.html\n");
+  system("./a.out-1 64.92291,77.78512 > map-bookmarks-users-20211217/20210118-2214-cafe-yesh-ka.html\n");
+  system("./a.out-1 64.47998,76.70336 > map-bookmarks-users-20211217/20210118-2217-cafe-milano.html\n");
+  system("./a.out-1 63.20458,75.45786 > map-bookmarks-users-20211217/20210118-2236-smovtrovaya-ploshchadka.html\n");
+  system("./a.out-1 62.25695,74.47956 > map-bookmarks-users-20211217/20210118-2304-victory-park.html\n");
+  system("./a.out-1 61.24285,73.41561 > map-bookmarks-users-20211217/20210118-2309-sayma-park.html\n");
+  system("./a.out-1 61.26585,75.19642 > map-bookmarks-users-20211217/20210118-2312-eagle-cafe.html\n");
+  system("./a.out-1 60.94083,76.54622 > map-bookmarks-users-20211217/20210118-2318-ugra-mall.html\n");
+  system("./a.out-1 59.62691,112.74904 > map-bookmarks-users-20211217/20210119-0422-blyuz-cafe.html\n");
+  system("./a.out-1 62.53823,113.97797 > map-bookmarks-users-20211217/20210119-0428-solyanka-cafe.html\n");
+  system("./a.out-1 66.41208,112.24744 > map-bookmarks-users-20211217/20210119-0437-anyutka-cafe.html\n");
+  system("./a.out-1 64.73516,177.51484 > map-bookmarks-users-20211217/20210119-0527-kafe-bar-studio.html\n");
+  system("./a.out-1 66.32486,-179.12084 > map-bookmarks-users-20211217/20210119-0535-13-stulyev.html\n");
+  system("./a.out-1 64.49998,-165.40963 > map-bookmarks-users-20211217/20210119-0732-bering-tea-coffee.html\n");
+  system("./a.out-1 71.28844,-156.78366 > map-bookmarks-users-20211217/20210119-0736-subway.html\n");
+  system("./a.out-1 67.25190,-150.17757 > map-bookmarks-users-20211217/20210119-0742-coldfoot-camp.html\n");
+  system("./a.out-1 0.28910,110.24939 > map-bookmarks-users-20211217/20210119-1320-kopitiam-jojiju.html\n");
+  system("./a.out-1 -0.03844,110.10783 > map-bookmarks-users-20211217/20210119-1325-tayan-island-bridge.html\n");
+  system("./a.out-1 0.84376,112.92318 > map-bookmarks-users-20211217/20210119-1328-enjoy-cafe.html\n");
+  system("./a.out-1 0.06231,111.48470 > map-bookmarks-users-20211217/20210119-1337-lokale-sintang.html\n");
+  system("./a.out-1 -0.96167,114.89253 > map-bookmarks-users-20211217/20210119-1349-maynd-coffee-cafe.html\n");
+  system("./a.out-1 4.25445,117.89667 > map-bookmarks-users-20211217/20210119-1407-eastern-plaza.html\n");
+  system("./a.out-1 1.47224,124.83191 > map-bookmarks-users-20211217/20210119-1543-manado-town-square.html\n");
+  system("./a.out-1 -5.14871,119.40572 > map-bookmarks-users-20211217/20210119-1549-globe-cpi-makassar.html\n");
+  system("./a.out-1 -3.73512,-38.56634 > map-bookmarks-users-20211217/20210120-0607-north-shopping-fortaleza.html\n");
+  system("./a.out-1 -2.42583,-54.75634 > map-bookmarks-users-20211217/20210120-0612-praia-do-maracana.html\n");
+  system("./a.out-1 6.82519,-58.15888 > map-bookmarks-users-20211217/20210120-0615-seawall-band-stand.html\n");
+  system("./a.out-1 5.16430,-52.63590 > map-bookmarks-users-20211217/20210120-0623-plage-de-la-cocoteraie.html\n");
+  system("./a.out-1 -7.32951,-34.79507 > map-bookmarks-users-20211217/20210120-0628-praia-do-coqueirinho.html\n");
+  system("./a.out-1 -27.60430,-48.46741 > map-bookmarks-users-20211217/20210120-0633-cafe-cultura.html\n");
+  system("./a.out-1 -36.54163,-56.68850 > map-bookmarks-users-20211217/20210120-0637-playa-santa-teresita.html\n");
+  system("./a.out-1 0.49557,25.20763 > map-bookmarks-users-20211217/20210120-1405-chutes-wagenia.html\n");
+  system("./a.out-1 -32.99976,17.95469 > map-bookmarks-users-20211217/20210120-1409-saldanha-beach.html\n");
+  system("./a.out-1 -33.34635,18.15441 > map-bookmarks-users-20211217/20210120-1413-yzerfontein-tourism-office.html\n");
+  system("rm a.out-1\n");
+}
+
